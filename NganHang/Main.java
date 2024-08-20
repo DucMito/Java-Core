87702c3989748a70928413d2f4b9786fd065db49
Đức <honghanh11062003@gmail.com>
2024-08-20 16:18:17 +0700

    .

--- NganHang/Main.java
+++ NganHang/Main.java
@@ -1,33 +1,36 @@
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Account account = new Account("techmaster", "hoclacoviec", 10000);
-        if (loginUser(account, scanner)){
-            boolean running = true;
-            while (running){
+        Account account = new Account("techmaster", "hoclacoviec", 100000);
+        if(loginUser(account, scanner)){
+            while (true){
                 choiceUser(account, scanner);
-                running = confirmUser(scanner);
+                System.out.println("Tiep tuc khong(Y/N):");
+                String retry = scanner.nextLine();
+                if(retry.equalsIgnoreCase("N")){
+                    System.out.println("thoat chuong tinh");
+                    System.exit(0);
+                }
             }
         }
-        scanner.close();
     }
 
     public static boolean loginUser(Account account, Scanner scanner){
-        while (true){
-            System.out.println("Nhap username: ");
+        while(true){
+            System.out.println("Nhap user: ");
             String inputUser = scanner.nextLine();
-            System.out.println("Nhap password: ");
+            System.out.println("Nhap pass: ");
             String inputPass = scanner.nextLine();
             if(account.getUsername().equals(inputUser) && account.getPassword().equals(inputPass)){
-                System.out.println("Login thanh cong");
+                System.out.println("Login successfully");
                 return true;
             }else{
-                System.out.println("Login that bai. Thu lai(Y/N)");
+                System.out.println("Login fail !! Again(Y/N)");
                 String retry = scanner.nextLine();
-                if(retry.equalsIgnoreCase("N")) {
+                if(retry.equalsIgnoreCase("N")){
                     System.out.println("Thoat chuong trinh");
                     System.exit(0);
                 }
@@ -36,41 +39,31 @@ public class Main {
     }
 
     public static void choiceUser(Account account, Scanner scanner){
-        System.out.println("Moi ban chon yeu cau: ");
-        System.out.println("1. Show profile: ");
+        System.out.println("Moi ban nhap: ");
+        System.out.println("1. Profile: ");
         System.out.println("2. Withdraw Money: ");
         int choice = Integer.parseInt(scanner.nextLine());
         switch (choice){
             case 1:
-                System.out.println("Username: " + account.getUsername());
+                System.out.println("Username: "  + account.getUsername());
                 System.out.println("Password: " + account.getPassword());
                 System.out.println("Balance: " + account.getBalance());
                 break;
             case 2:
-                System.out.println("Nhap so tien rut: ");
+                System.out.println("So tien ban muon rut: ");
                 double inputBalance = Double.parseDouble(scanner.nextLine());
                 if(inputBalance <= account.getBalance()){
                     account.setBalance(account.getBalance() - inputBalance);
-                    System.out.println("So tien da rut: " + inputBalance + "con lai: " + account.getBalance() + "Vao luc:  " + java.time.LocalDateTime.now());
+                    System.out.println("So tien da rut: " + inputBalance + "Vao luc: " + java.time.LocalDateTime.now() + "So du: " +account.getBalance());
                 }else{
-                    System.out.println("So tien khong du");
-                    break;
+                    System.out.println("So du khong du.");
                 }
+                break;
             default:
-                System.out.println("Lua chon lai");
+                System.out.println("Moi ban nhap lai lua chon!!");
 
         }
-
     }
 
-    public static boolean confirmUser(Scanner scanner){
-        System.out.println("Co tiep tuc(Y/N): ");
-        String continueChoice = scanner.nextLine();
-        if(continueChoice.equalsIgnoreCase("N")){
-            System.out.println("Thoat chuong trinh.");
-            System.exit(0);
-        }
-        return true;
-    }
 
 }
